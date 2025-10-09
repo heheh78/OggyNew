aea64113ab971ea6d5f99039dfd9941059e6ae24
Leo MC <nisanox34@gmail.com>
2025-10-09 20:44:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -72,7 +72,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo="https://i.ibb.co/CKV4Qd71/IMG-20250926-205407-612.jpg",
+            photo="https://i.imghippo.com/files/XGLG7861zeg.jpg",
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
@@ -158,7 +158,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo="https://i.ibb.co/CKV4Qd71/IMG-20250926-205407-612.jpg",
+            photo="https://i.imghippo.com/files/XGLG7861zeg.jpg",
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
